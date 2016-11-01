705381fde1d3c80231953c46d03a8087cfb1fc4f
eljam <guillaume.cavana@gmail.com>
2016-11-01 09:33:40 +0100

    Initial import

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import classes from './Dashboard.scss'
+
+export const Dashboard = (props) => {
+  const listJSX = props.dashboard.dashboardItems.map((item, i) => {
+    return <h4 key={i}>{item.label}</h4>
+  })
+
+  return (
+    <div>
+      <h2 className={classes.dashboardContainer}>
+        Dashboard visits:
+        {' '}
+        <span className={classes['dashboard--green']}>
+          {props.dashboard.visitsCount}
+        </span>
+      </h2>
+      {listJSX}
+    </div>
+  )
+}
+
+Dashboard.propTypes = {
+  dashboard: React.PropTypes.object.isRequired
+}
+
+export default Dashboard
---
+++ src/routes/Dashboard/containers/DashboardContainer.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import {
+  dashboardVisitIncrement,
+  dashboardAddItem,
+  dashboardEditItem
+} from '../modules/dashboard'
+
+/*  This is a container component. Notice it does not contain any JSX,
+    nor does it import React. This component is **only** responsible for
+    wiring in the actions and state necessary to render a presentational
+    component - in this case, the dashboard:   */
+
+import Dashboard from 'components/Dashboard'
+
+/*  Object of action creators (can also be function that returns object).
+    Keys will be passed as props to presentational components. Here we are
+    implementing our wrapper around increment; the component doesn't care   */
+
+const mapActionCreators = {
+  dashboardVisitIncrement: () => dashboardVisitIncrement(1),
+  dashboardAddItem: (value) => dashboardAddItem(value),
+  dashboardEditItem: (value) => dashboardEditItem(value)
+}
+
+const mapStateToProps = (state) => ({
+  dashboard: state.dashboard
+})
+
+class DashboardContainer extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.inputOnChange = this.inputOnChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+    this.itemOnEdit = this.itemOnEdit.bind(this)
+
+    this.state = {
+      inputValue: '',
+      editedItemIndex: null
+    }
+  }
+
+  componentDidMount () {
+    this.props.dashboardVisitIncrement();
+  }
+
+  inputOnChange (e) {
+    this.setState({inputValue: e.target.value})
+  }
+
+  itemOnEdit (itemIndex) {
+    const editedItem =  this.props.dashboard.dashboardItems[itemIndex]
+    this.setState({inputValue: editedItem.value, editedItemIndex: itemIndex})
+  }
+
+  onSubmit (e) {
+    e.preventDefault()
+    const val = this.state.inputValue
+    const editedItemIndex = this.state.editedItemIndex
+    if (val && editedItemIndex !== null) {
+      this.props.dashboardEditItem({val, editedItemIndex})
+      this.setState({inputValue: '', editedItemIndex: null})
+    } else if (val){
+      this.props.dashboardAddItem(val)
+      this.setState({inputValue: ''})
+    } else {
+      alert('Value cannot be empty')
+    }
+  }
+
+  render () {
+    return (
+      <Dashboard {...this.props}
+        editedItemIndex={this.state.editedItemIndex}
+        itemOnEdit={this.itemOnEdit}
+        inputValue={this.state.inputValue}
+        inputOnChange={this.inputOnChange}
+        onSubmit={this.onSubmit} />
+    )
+  }
+}
+
+/*  Note: mapStateToProps is where you should use `reselect` to create selectors, ie:
+
+    import { createSelector } from 'reselect'
+    const dashboard = (state) => state.dashboard
+    const tripleCount = createSelector(dashboard, (count) => count * 3)
+    const mapStateToProps = (state) => ({
+      dashboard: tripleCount(state)
+    })
+
+    Selectors can compute derived data, allowing Redux to store the minimal possible state.
+    Selectors are efficient. A selector is not recomputed unless one of its arguments change.
+    Selectors are composable. They can be used as input to other selectors.
+    https://github.com/reactjs/reselect    */
+
+export default connect(mapStateToProps, mapActionCreators)(DashboardContainer)
